7a793bfc5d0bfceb38c398a4571365aaf44efdbd
“Foxshane997” <Foxshane997@gmail.com>
2024-06-05 21:00:54 -0700

    Added files & their paths

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MoleContainer from './Components/MoleContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React a Mole!</h1>
+      <MoleContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Mole.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Mole(){
+    return(
+        <div>
+            <h2>This is a Mole.</h2>
+        </div>
+    )
+}
+
+export default Mole
